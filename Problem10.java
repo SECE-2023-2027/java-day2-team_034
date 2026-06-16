768556b1c200f83f98459657ecd764b8e024ba15
sanjay.s2023csbs@sece.ac.in <sanjay.s2023csbs@sece.ac.in>
2026-06-16 19:07:41 +0530

    assignment2

---
+++ Problem10.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Problem10 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter array size: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter array elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        if (arr[0] == arr[n - 1]) {
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
+    }
+}
